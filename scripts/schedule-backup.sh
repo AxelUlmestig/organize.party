162c83ebae8c781ade16445aae7190ae718416b5
Axel Ulmestig <axel.ulmestig@gmail.com>
2025-02-15 20:35:10 +0100

    fix cron script to not overwrite existing config

--- scripts/schedule-backup.sh
+++ scripts/schedule-backup.sh
@@ -4,11 +4,12 @@ set -e
 
 CRON_CONFIG="0 3 * * * cd $PWD && make backup-db"
 EXISTING_CONFIG="$(crontab -l)"
+LINE_BREAK=$'\n'
 
 if echo "$EXISTING_CONFIG" | grep -cFxq "$CRON_CONFIG"; then
   echo "crontab already configured"
   exit 0
 fi
 
-echo "0 3 * * * cd $PWD && make backup-db" | crontab -
+echo "$EXISTING_CONFIG$LINE_BREAK$CRON_CONFIG" | crontab -
 echo "crontab updated"
