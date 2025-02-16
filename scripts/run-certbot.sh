70926ac141ac2a77eea967a21936977a3d93b8e5
Axel Ulmestig <AxelUlmestig@users.noreply.github.com>
2025-02-16 15:47:35 +0100

    certbot setup script (#35)

---
+++ scripts/run-certbot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -x
+
+docker compose down nginx
+docker compose up nginx-certbot -d
+
+docker compose run \
+  --rm \
+  certbot certonly \
+  --webroot \
+  --webroot-path /var/www/certbot/ \
+  -d organize.party
+
+docker compose down nginx-certbot
+docker compose up nginx -d
