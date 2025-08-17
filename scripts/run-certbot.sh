e69d6fb1a711630dd7c9ce679a40f781a45747a8
Axel Ulmestig <axel.ulmestig@gmail.com>
2025-08-17 14:58:46 +0100

    update certbot script

--- scripts/run-certbot.sh
+++ scripts/run-certbot.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 set -x
 
-docker compose down nginx
-docker compose up nginx-certbot -d
+# docker compose down nginx
+# docker compose up nginx-certbot -d
+
+# docker compose run \
+#   --rm \
+#   certbot certonly \
+#   --webroot \
+#   --webroot-path /var/www/certbot/ \
+#   -d organize.party
 
 docker compose run \
   --rm \
   certbot certonly \
   --webroot \
   --webroot-path /var/www/certbot/ \
-  -d organize.party
+  --non-interactive \
+  --email axel.ulmestig@gmail.com \
+  --agree-tos \
+  --no-eff-email \
+  --domains organize.party
 
-docker compose down nginx-certbot
-docker compose up nginx -d
+
+# docker compose down nginx-certbot
+# docker compose up nginx -d
+
+# we need to reload nginx after updating the certificate files, it's better to reload than to restart to avoid downtime. This hasn't been tested yet
+#
+# docker compose exec nginx nginx -s reload
