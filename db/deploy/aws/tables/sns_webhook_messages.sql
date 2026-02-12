918e8727867e20a4560170ab84c4f82807c8815a
Axel Ulmestig <axel.ulmestig@gmail.com>
2026-02-12 22:30:11 +0100

    check id aws sns webhook to prevent double processing

--- db/deploy/aws/tables/sns_webhook_messages.sql
+++ db/deploy/aws/tables/sns_webhook_messages.sql
@@ -6,13 +6,22 @@ BEGIN;
     id uuid not null default uuidv7(),
     received_at timestamptz not null default now(),
     state_machine_id bigint not null,
+    aws_sns_id text not null,
     materialized_status text not null default 'queued',
     contents jsonb not null,
 
     primary key (id)
   );
 
-  create index if not exists idx_aws_sns_webhook_messages_state_machine_id
+  create unique index if not exists idx_aws_sns_webhook_messages_state_machine_id
     on aws.sns_webhook_messages (state_machine_id);
+
+  create unique index if not exists idx_aws_sns_webhook_messages_aws_sns_id
+    on aws.sns_webhook_messages (aws_sns_id);
+
+  comment on  column aws.sns_webhook_messages.aws_sns_id is
+  $$
+  The ID given by AWS so we can detect if we get the same message sent to us twice
+  $$;
 
 COMMIT;
--- op-webapi/src/Op/WebAPI/Endpoints/IncomingWebhooks/AwsSns.hs
+++ op-webapi/src/Op/WebAPI/Endpoints/IncomingWebhooks/AwsSns.hs
@@ -6,10 +6,12 @@ import           Control.Monad.Except    (MonadError (..))
 import           Control.Monad.IO.Class  (MonadIO (liftIO))
 import           Control.Monad.Reader    (MonadReader)
 import qualified Data.Aeson              as Aeson
+import           Data.Aeson.Lens
 import qualified Data.ByteString.Lazy    as LBS
 import           Data.String.Interpolate (i)
 import           Data.Text               (Text)
 import           Data.Text.Encoding      (encodeUtf8)
+import           RIO                     ((^?))
 import           Servant                 (ServerError (..), err500)
 
 import qualified Op.Db                   as Db
@@ -30,12 +32,20 @@ handleAwsSnsWebhook rawRequestBody = do
           liftIO $ print errorMessage
           throwError err500 { errBody = errorMessage }
 
+  messageId <- do
+    case requestBody ^? key "MessageId" . _String of
+      Just messageId -> pure messageId
+      Nothing -> do
+          let errorMessage = [i|Unexpected AWS SES request, expected MessageId in request: #{rawRequestBody}|]
+          liftIO $ print errorMessage
+          throwError err500 { errBody = errorMessage }
+
   liftIO $ putStrLn [i|Received webhook from AWS SES: #{Aeson.encode requestBody}|]
 
   Db.queryDbOr Db.printAndThrow500 do
     Db.statement
-      requestBody
+      (requestBody, messageId)
       [Db.resultlessStatement|
-      insert into aws.sns_webhook_messages (contents)
-      values ($1::jsonb)
+      insert into aws.sns_webhook_messages (contents, aws_sns_id)
+      values ($1::jsonb, $2::text)
       |]
