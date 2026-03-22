37f3775c3446332a43afbed4fd2c3b8d99626c73
Axel Ulmestig <axel.ulmestig@gmail.com>
2026-03-22 13:44:34 +0100

    remove Email module

--- op-webapi/src/Op/WebAPI/Endpoints/InitForgetMeRequest.hs
+++ op-webapi/src/Op/WebAPI/Endpoints/InitForgetMeRequest.hs
@@ -3,14 +3,13 @@
 module Op.WebAPI.Endpoints.InitForgetMeRequest (initForgetMe) where
 
 import           Control.Monad.Except            (MonadError)
-import           Control.Monad.IO.Class          (MonadIO)
-import           Control.Monad.Reader            (MonadReader, asks)
+import           Data.String.Interpolate         (__i)
 import qualified Data.UUID                       as UUID
-import qualified Hasql.Session                   as Hasql
+import           RIO                             (MonadIO, MonadReader, Text,
+                                                  asks)
 import           Servant                         (ServerError)
 
 import qualified Op.Db                           as Db
-import qualified Op.WebAPI.Email                 as Email
 import           Op.WebAPI.Types.AppEnv          (AppEnv (..))
 import           Op.WebAPI.Types.ForgetMeRequest (InitForgetMeInput (..),
                                                   InitForgetMeResult (..))
@@ -20,10 +19,10 @@ initForgetMe ::
   InitForgetMeInput ->
   m InitForgetMeResult
 initForgetMe InitForgetMeInput{email} = do
-  (forgetMeRequestId, email) <- Db.queryDbOr Db.printAndThrow500 (Hasql.statement email statement)
+  forgetMeRequestId <- Db.queryDbOr Db.printAndThrow500 (Db.statement email statement)
 
   hostUrl' <- asks hostUrl
-  Email.sendForgetMeConfirmation Db.printAndThrow500 hostUrl' forgetMeRequestId email
+  sendForgetMeConfirmation Db.printAndThrow500 hostUrl' forgetMeRequestId email
 
   pure $ InitForgetMeResult
     { initForgetMeResultEmail = email
@@ -42,12 +41,51 @@ initForgetMe InitForgetMeInput{email} = do
             email
         )
 
-        select id::uuid, email::text
+        select id::uuid
         from inserted
 
         union
 
-        select id::uuid, email::text
+        select id::uuid
         from forgetme_requests
         where email = $1::text
      |]
+
+
+sendForgetMeConfirmation ::
+  (Db.HasDbConnection env, MonadIO m, MonadReader env m) =>
+  (Db.SessionError -> m ()) ->
+  String ->
+  UUID.UUID ->
+  Text ->
+  m ()
+sendForgetMeConfirmation onError hostUrl forgetMeRequestId email = do
+  let subject = "Forget me request"
+  Db.queryDbOr onError (Db.statement (email, subject, body) statement)
+  where
+    body =
+      [__i|
+        A request to delete your data has been received. If you did not make
+        this request, please ignore this email.
+        <br>
+        <br>
+        If you did make this request, please click the link below to confirm. <b>Warning: this will delete all your data, it cannot be undone</b>
+        <br>
+        <a href="#{hostUrl}/forget-me/#{forgetMeRequestId}">#{hostUrl}/forget-me/#{forgetMeRequestId}</a>
+        <br>
+        <br>
+        It will not delete events created by you, there's no connection between email addresses and events. It's impossible to tell which ones were created by you.
+      |]
+    statement =
+      [Db.resultlessStatement|
+        insert into email.emails (
+          recipient_email,
+          subject,
+          body
+        )
+        values (
+          $1::text,
+          $2::text,
+          $3::text
+        )
+      |]
