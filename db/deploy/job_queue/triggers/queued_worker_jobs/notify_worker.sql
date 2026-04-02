c2c2feefd2bdb30dda777333d7a1481bf642612c
Axel Ulmestig <axel.ulmestig@gmail.com>
2026-04-02 13:40:59 +0200

    fix faulty logic in 'msUntilRunAt'

--- db/deploy/job_queue/triggers/queued_worker_jobs/notify_worker.sql
+++ db/deploy/job_queue/triggers/queued_worker_jobs/notify_worker.sql
@@ -9,14 +9,14 @@ BEGIN;
       'new_worker_job',
       jsonb_build_object(
         'microsecondsUntilRunAt',
-        (extract(epoch from now() - new.run_at) * 1000000)::int
+        (extract(epoch from new.run_at - now()) * 1000000)::int
       )::text
     );
     return new;
   end;
   $$ language plpgsql;
 
-  create trigger trigger_notify_worker
+  create or replace trigger trigger_notify_worker
   after insert on job_queue.queued_worker_jobs
   for each row
   execute function job_queue.notify_worker();
